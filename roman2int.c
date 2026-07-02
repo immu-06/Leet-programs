c8856e9202192862240c85250641c868b392ccfb
immu-06 <forfinance3113@gmail.com>
2026-07-02 11:23:53 +0530

    Create roman2int.c
    roman to integer using c program

---
+++ roman2int.c
@@ -0,0 +1,30 @@
+#include <string.h>
+int getRomanValue(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default:  return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int total = 0;
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++) {
+        int current = getRomanValue(s[i]);
+        
+        if (i + 1 < length && current < getRomanValue(s[i + 1])) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
