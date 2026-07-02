0f1968f0c97874f9dc6c06d794cd6e8bc3b9d6eb
immu-06 <forfinance3113@gmail.com>
2026-07-02 11:17:23 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,19 +1,16 @@
 #include <stdbool.h>
 
 bool isPalindrome(int x) {
-    // Negative numbers and numbers ending in 0 (except 0 itself) cannot be palindromes
+
     if (x < 0 || (x % 10 == 0 && x != 0)) {
         return false;
     }
 
     int reversed_half = 0;
     while (x > reversed_half) {
-        // Pop the last digit from x and push it to reversed_half
         reversed_half = (reversed_half * 10) + (x % 10);
         x /= 10;
     }
 
-    // For even digits: x == reversed_half
-    // For odd digits: x == reversed_half / 10 (removes the middle digit)
     return x == reversed_half || x == reversed_half / 10;
 }
